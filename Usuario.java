e16d1e6544e2e02c717281729bf9b4ec21983fa5
thomazgottliebs <thomazcorrea4@gmail.com>
2026-06-29 18:35:52 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,7 +1,16 @@
-public class Usuario{
+public class Usuario {
 
-private String Name;
+    private String nome;
+    private String email;
+    private String senha;
+    private String perfil; // Coordenador, Professor, etc.
 
-private double Saldo;
+    public Usuario(String nome, String email, String senha, String perfil) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
 
+    // getters e setters
 }
