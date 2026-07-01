b0087c1dea76e9a5e26e6539d8650b3bb97048e3
From999Hell <ramone9414@hotmail.com>
2026-07-01 17:01:39 -0300

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Grade {
+
+    private List<Alocacao> alocacoes;
+
+    public Grade() {
+        alocacoes = new ArrayList<>();
+    }
+
+    public void adicionarAlocacao(Alocacao alocacao) {
+        alocacoes.add(alocacao);
+    }
+
+    public List<Alocacao> getAlocacoes() {
+        return Collections.unmodifiableList(alocacoes);
+    }
+
+}
