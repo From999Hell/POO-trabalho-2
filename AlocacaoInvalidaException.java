f3be6d66a6f4fd26ff8970d44193154a0b8234c7
From999Hell <ramone9414@hotmail.com>
2026-07-01 20:48:22 -0300

    Create AlocacaoInvalidaException.java

---
+++ AlocacaoInvalidaException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class AlocacaoInvalidaException extends RuntimeException {
+
+    public AlocacaoInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
